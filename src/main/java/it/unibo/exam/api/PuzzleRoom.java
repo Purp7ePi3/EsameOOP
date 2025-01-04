eb2dc5a36cc3b6fd91aadab7be4e0fae4372ac3a
TommasoNori <tommaso.nori@studio.unibo.it>
2025-01-04 17:36:36 +0100

    removefile

--- src/main/java/it/unibo/exam/api/PuzzleRoom.java
+++
@@ -1,9 +0,0 @@
-package it.unibo.exam.api;  // Changed from inteface to api
-
-import java.awt.Graphics2D;
-
-public interface PuzzleRoom {
-    void updatePuzzleLogic(KeyHandler keyHandler);
-    boolean isPuzzleSolved();
-    void draw(Graphics2D g2);
-}
